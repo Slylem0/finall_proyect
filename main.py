747640ca00b8aa8b86a3a8fa20143709eeb726b6
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2024-05-21 17:02:24 -0500

    u

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ frame.pack()
 #getting de information of the user
 
 get_data_of_fly = tkinter.LabelFrame(frame, text="Get data of fly")
-get_data_of_fly.grid(row = 0, column = 0, padx = 20, pady = 20)
+get_data_of_fly.grid(row = 0, column = 2, padx = 20, pady = 20)
 
 informatio1 = tkinter.Label(get_data_of_fly, text="Solo ida")
 informatio1.grid (row = 0, column = 0) 
 
+space = tkinter.Label(get_data_of_fly, text=" Holaaaa")
+space.grid (row = 0, column = 1)
 information2 = tkinter.Label (get_data_of_fly, text="Ocuppants")
 information2.grid(row = 0, column = 2)
 
-ocuppants_combobox = ttk.Combobox(get_data_of_fly, values = [1,2,3,4,5,6,7,8,9,10, "More?, Write it: "])
+ocuppants_combobox = tkinter.Spinbox(get_data_of_fly, from_ = 1, to = 72)
 ocuppants_combobox.grid(row = 1, column = 2)
 
 
