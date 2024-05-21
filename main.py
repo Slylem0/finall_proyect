7183cf9669a067f4d9255f7abe56aa9feb602c60
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2024-05-21 16:23:11 -0500

    actu

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ archivo = open("C:/Users/pnico/Documents/Finall_proyect/finall_proyect/Datos_Vue
 
 window = tkinter.Tk()
 window.title("Airline check-in system")
+frame = tkinter.Frame(window)
+frame.pack()
 
+#getting de information of the user
+get_data_of_fly = tkinter.LabelFrame(frame, text="Get data of fly")
+get_data_of_fly.grid(row = 0, column = 0)
+
+
+informatio1 = tkinter.Label(get_data_of_fly, text="Solo ida")
+informatio1.grid (row = 0, column = 0) 
+
+information2 = tkinter.Label (get_data_of_fly, text="1")
+information2.grid(row = 0, column = 2)
 window.mainloop()
 
