9f473f45fae525a77cc9a528d5d54a8d083650f4
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2024-05-21 16:02:59 -0500

    update main

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter 
+
+archivo = open("C:/Users/pnico/Documents/Finall_proyect/finall_proyect/Datos_Vuelos _Finales.txt", "r")
+
+
+window = tkinter.Tk()
+window.title("Airline check-in system")
+
+window.mainloop()
+
