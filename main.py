5996b42220405ca01acab568d74ff43ac82f2f2d
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2024-05-25 21:31:54 -0500

    hello

--- main.py
+++ main.py
@@ -3,19 +3,31 @@ import customtkinter
 from PIL import ImageTk, Image
 
 def first_window():
-    #The apparence of the window set to black
+    #The apparence of the window set to the system mode
     customtkinter.set_appearance_mode ("system")
     customtkinter.set_default_color_theme ("green")
 
     app = customtkinter.CTk()
     app.title("My App")
-    app.geometry("800x600")
+    app.geometry("600x500")
 
     #The background image is set
-    image_background = ImageTk.PhotoImage(Image.open("background.jpg", "r"))
-    l1 = customtkinter.CTkLabel(master=app, image=image_background)
-    l1.pack()
+    ########################################
+    # image_background = ImageTk.PhotoImage(Image.open("background.jpg", "r"))
+    # l1 = customtkinter.CTkLabel(master=app, image=image_background)
+    # l1.pack()
+    ########################################
 
+
+    #now we gona do a frame 
+
+    frame = customtkinter.CTkFrame(app)
+    frame.place (relx = 0.5, rely = 0.5, anchor = "center")
+
+    #now we goona do the options for the user
+
+    option1 = customtkinter.CTkLabel(master=frame, text= "Welcome, pls select an option", font = ("Century Gothic", 15))
+    option1.place ( x = 10, y = 45)
     app.mainloop()
 
 
