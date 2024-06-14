23e08f43e999eafae446ae1bf6eee1f24b82173a
Nandojg24 <juanferjg24@gmail.com>
2024-06-13 21:18:19 -0500

    Asi se sube un archivo

--- Nando doc con links del compu.py
+++ Nando doc con links del compu.py
@@ -1,53 +1,361 @@
-# Import all libraries we gonna use
 import tkinter
 import customtkinter
 from PIL import ImageTk, Image
-from tkinter import PhotoImage, OptionMenu, Listbox, messagebox
 from tkinter import *
-import tabulate
+from tkinter import OptionMenu, messagebox, Listbox, PhotoImage, StringVar
+import tkinter.messagebox as messagebox
+import random
 
 
-# Starting making the functions that we will use
-def get_data_flyes(origin, destine, users, date):
-    print(origin.cget("text") + "\n" +
-          destine.cget("text") + "\n" +
-          users.get() + "\n" + date.cget("text"))
+def Ticket():
 
-    return (origin.cget("text") + "\n" +
-            destine.cget("text") + "\n" +
-            users.get() + "\n" + date.cget("text"))
+    with open("Codigos_Vuelos.txt", "a") as archivo:
+
+        window9 = customtkinter.CTk()
+        window9.title("Ticket")
+        window9.geometry("600x440")
+        window9.resizable(False, False)
+        alfabhet = ["A", "B", "C", "D", "E", "F",
+                    "G", "H", "I", "J", "K", "L",
+                    "M", "N", "O", "P", "Q", "R",
+                    "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+        first = random.choice(alfabhet)
+        first += "-"
+
+        frame = customtkinter.CTkFrame(master=window9, width=500,
+                                       height=450, corner_radius=15)
+        frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        for i in range(1, 4):
+            first += random.choice(alfabhet)
+            first += random.choice(numbers)
+
+        archivo.write(first)
+        archivo.write("\n")
+
+        archivo.close()
+
+        customtkinter.CTkLabel(master=frame, text="Ticket", font=(
+            "Century Gothic", 15)).place(x=300, y=100)
+        customtkinter.CTkLabel(master=frame, text=f"{first}", font=(
+            "Century Gothic", 15)).place(x=300, y=150)
+
+        window9.mainloop()
 
 
-def botton1_click():
-    window3 = customtkinter.CTk()
-    window3.title("Chek-in")
-    window3.geometry("600x440")
-    window3.resizable(False, False)
+def comprobate_payment(card_number, cvv, window6):
 
-    frame = customtkinter.CTkFrame(master=window3, width=475,
-                                   height=375, corner_radius=15)
+    card_number = card_number.get()
+    cvv = cvv.get()
+
+    # Directly check the length of the card number
+    if len(card_number) == 16:
+        # Directly check the length of the CVV
+        if len(cvv) == 3:
+            customtkinter.CTkLabel(
+                master=window6, text="The payment was successful",
+                font=("Century Gothic", 16)).place(x=270, y=140)
+            Ticket()
+        else:
+            customtkinter.CTkLabel(
+                master=window6, text="The CVV is incorrect",
+                font=("Century Gothic", 16)).place(x=270, y=140)
+    else:
+        customtkinter.CTkLabel(
+            master=window6, text="The card number is incorrect",
+            font=("Century Gothic", 16)).place(x=270, y=140)
+
+    window6.mainloop()
+
+
+def card_payment():
+    try:
+        window6 = customtkinter.CTk()
+        window6.title("Payment")
+        window6.geometry("600x440")
+        window6.resizable(False, False)
+
+        frame = customtkinter.CTkFrame(master=window6, width=500,
+                                       height=450, corner_radius=15)
+        frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        customtkinter.CTkLabel(
+            master=frame, text="Card Number",
+            font=("Century Gothic", 15)).place(x=10, y=10)
+        card_number = customtkinter.CTkEntry(
+            master=frame, width=190,
+            font=("Century Gothic", 15), corner_radius=40)
+        card_number.place(x=10, y=50)
+
+        customtkinter.CTkLabel(
+            master=frame, text="Expiration Date", 
+            font=("Century Gothic", 15)).place(x=300, y=10)
+        months = ["January", "February", "March", "April",
+                  "May", "June", "July", "August",
+                  "September", "October", "November", "December"]
+        months_variable = StringVar(frame)
+        months_variable.set(months[0])
+        drop = OptionMenu(frame, months_variable, *months)
+        drop.place(x=400, y=50)
+
+        years = [i for i in range(2025, 2040)]
+        years_variable = StringVar(frame)
+        years_variable.set(years[0])
+        drop2 = OptionMenu(frame, years_variable, *years)
+        drop2.place(x=500, y=50)
+
+        customtkinter.CTkLabel(
+            master=frame, text="CVV", 
+            font=("Century Gothic", 15)).place(x=10, y=100)
+        cvv = customtkinter.CTkEntry(master=frame, width=150,
+                                     font=("Century Gothic", 15))
+        cvv.place(x=10, y=150)
+
+        customtkinter.CTkButton(
+            master=frame, text="Pay", corner_radius=6,
+            command=lambda: comprobate_payment(
+                card_number, cvv, window6)).place(x=250, y=200)
+
+        window6.mainloop()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def comprobate_user(first_name, last_name, email, phone, window5):
+    global names
+
+    correo = email.get()
+    phone = phone.get()
+    first_name = first_name.get()
+    last_name = last_name.get()
+
+    names = [first_name, last_name]
+
+    contador = 0
+    for i in correo:
+        if i == "@":
+            contador += 1
+        elif contador == 1 and i == ".":
+            contador += 1
+    if contador == 2:
+        if len(phone) == 10:
+            card_payment()
+        else:
+            customtkinter.CTkLabel(
+                master=window5, text="Phone number is incorrect",
+                font=("Century Gothic", 10)).place(x=250, y=10)
+            window5.mainloop()
+
+    window5.mainloop()
+
+
+def _get_data():
+    window5 = customtkinter.CTk()
+    window5.title("Data")
+    window5.geometry("600x440")
+    window5.resizable(False, False)
+
+    frame = customtkinter.CTkFrame(
+        master=window5, width=500, height=450, corner_radius=15)
     frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-    customtkinter.CTkLabel(master=frame, text="Check-in",
-                           font=("Century Gothic", 25)).place(x=180, y=10)
+    customtkinter.CTkLabel(
+        master=frame, text="First Name",
+        font=("Century Gothic", 15)).place(x=10, y=10)
+    first_name = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    first_name.place(x=10, y=50)
 
-    customtkinter.CTkLabel(master=frame,
-                           text="Write the Number of your ticket",
-                           font=("Centuary Gothic", 15)).place(x=100, y=50)
+    customtkinter.CTkLabel(
+        master=frame, text="Last Name",
+        font=("Century Gothic", 15)).place(x=400, y=10)
+    last_name = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    last_name.place(x=350, y=50)
 
-    user_data = customtkinter.CTkEntry(master=frame,
-                                       width=150,
-                                       font=("Century Gothic", 15))
-    user_data.place(x=180, y=100)
+    customtkinter.CTkLabel(
+        master=frame, text="Gender",
+        font=("Century Gothic", 15)).place(x=10, y=100)
+    gender = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    gender.place(x=10, y=150)
 
-    botton_ckeck = customtkinter.CTkButton(master=frame,
-                                           text="Check",
-                                           corner_radius=10)
-    botton_ckeck.place(x=180, y=150)
+    customtkinter.CTkLabel(
+        master=frame, text="Nacionality",
+        font=("Century Gothic", 15)).place(x=400, y=100)
+    nacionality = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    nacionality.place(x=350, y=150)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Number ID",
+        font=("Century Gothic", 15)).place(x=10, y=200)
+    number_id = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    number_id.place(x=10, y=250)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Born Date",
+        font=("Century Gothic", 15)).place(x=400, y=200)
+    born_date = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    born_date.place(x=350, y=250)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Asistence fly",
+        font=("Century Gothic", 15)).place(x=10, y=300)
+    asistence_fly = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    asistence_fly.place(x=10, y=350)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Email",
+        font=("Century Gothic", 15)).place(x=400, y=300)
+    email = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    email.place(x=350, y=350)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Phone",
+        font=("Century Gothic", 15)).place(x=10, y=380)
+    phone = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 15))
+    phone.place(x=10, y=410)
+
+    customtkinter.CTkButton(
+        master=frame, text="Buy",
+        corner_radius=6, command=lambda: comprobate_user(
+            first_name, last_name, email, phone, window5)).place(x=350, y=410)
+    window5.mainloop()
+
+
+def buy_ticket(category):
+    global final_seat
+    category = category
+
+    window4 = customtkinter.CTk()
+    window4.title("Seats")
+    window4.geometry("600x440")
+    window4.resizable(False, False)
+
+    # Marco para los asientos
+    frame_asientos = customtkinter.CTkFrame(
+        master=window4, width=475, height=375, corner_radius=15)
+    frame_asientos.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    # Agregar letras en la parte superior de las columnas
+    letras = ['A', 'B', 'C', 'D', 'E', 'F']
+    for j in range(6):
+        customtkinter.CTkLabel(
+            master=frame_asientos, text=letras[j]).grid(row=0, column=j+1)
+
+    matrix_asientos = [[0 for i in range(6)] for j in range(12)]
+
+    # Comenzar en 1 para ajustar por las etiquetas de letras
+    for i in range(1, 13):
+        # Enumerar las filas
+        customtkinter.CTkLabel(
+            master=frame_asientos, text=str(i)).grid(row=i, column=0)
+        for j in range(6):
+            matrix_asientos[i-1][j] = customtkinter.CTkButton(
+                master=frame_asientos, text="Asiento",
+                corner_radius=1, width=20, height=20)
+            matrix_asientos[i-1][j].grid(row=i, column=j+1)
+            # Ajustar índices para las etiquetas
+
+    customtkinter.CTkLabel(
+        master=window4, text="Seat",
+        font=("Century Gothic", 15)).place(x=10, y=100)
+
+    customtkinter.CTkButton(
+        master=window4, text="Select",
+        corner_radius=6, command=_get_data).place(x=10, y=300)
+
+    if category == "Aluminium":
+        customtkinter.CTkLabel(
+            master=window4, text="Your Seat is Aleatory",
+            font=("Century Gothic", 13)).place(x=5, y=150)
+        column = ["A", "B", "C", "D", "E", "F"]
+        b = {column[random.randint(0, 5)]}
+        a = random.randint(1, 12)
+        seat = f"{b}-{a}"
+        final_seat = customtkinter.CTkLabel(
+            master=window4, text=f"{seat}",
+            font=("Century Gothic", 12)).place(x=5, y=200)
+
+    elif category == "Diamond":
+        customtkinter.CTkLabel(
+            master=window4, text="Your file is aleatory",
+            font=("Century Gothic", 13)).place(x=10, y=150)
+        column = ["A", "B", "C", "D", "E", "F"]
+        a = random.randint(1, 12)
+        final_seat = customtkinter.CTkLabel(
+            master=window4, text=f"Your file is {random.randint(1, 12)}",
+            font=("Century Gothic", 12)).place(x=5, y=200)
+
+        customtkinter.CTkEntry(
+            master=window4, width=150,
+            font=("Century Gothic", 15)).place(x=10, y=250)
+
+    elif category == "Premium":
+        customtkinter.CTkLabel(
+            master=window4, text="You can select the seat",
+            font=("Century Gothic", 10)).place(x=5, y=150)
+        final_seat = customtkinter.CTkEntry(
+            master=window4, width=150,
+            font=("Century Gothic", 15)).place(x=10, y=200)
+
+    customtkinter.CTkButton(
+        master=window4, text="Buy",
+        corner_radius=6, command=_get_data).place(x=10, y=300)
+
+    window4.mainloop()
+
+
+def comprube_data(code, window3):
+    with open("Codigos_Vuelos.txt", "w") as archivo:
+        for i in archivo:
+            if i == code:
+                customtkinter.CTkLabel(
+                    master=window3,
+                    text="The code is correct, You have done Check-in",
+                    font=("Century Gothic", 16)).place(x=270, y=140)
+            else:
+                customtkinter.CTkLabel(
+                    master=window3,
+                    text="The code is incorrect",
+                    font=("Century Gothic", 16)).place(x=270, y=140)
+
+
+def button1_click():
+
+    window3 = customtkinter.CTk()
+    window3.title("Chek-in")
+    window3.geometry("600x500")
+    window3.resizable(False, False)
+
+    frame = customtkinter.CTkFrame(
+        master=window3, width=475, height=450, corner_radius=15)
+    frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    customtkinter.CTkLabel(
+        master=frame, text="Number of Code",
+        font=("Century Gothic", 20)).place(x=150, y=150)
+    code = customtkinter.CTkEntry(
+        master=frame, width=150, font=("Century Gothic", 10))
+    code.place(x=150, y=220)
+
+    button4 = customtkinter.CTkButton(
+        master=frame, text="Check",
+        corner_radius=6, command=lambda: [comprube_data(code.get(), window3)])
+    button4.place(x=150, y=300)
+
     window3.mainloop()
 
 
-def button2_click():
+def botton2_click():
     window2 = customtkinter.CTk()
     window2.title("Buy a ticket")
     window2.geometry("600x500")
@@ -262,7 +570,7 @@ def button2_click():
             botton_more_info.place(x=10, y=80)
             botton_aluminium = customtkinter.CTkButton(
                 master=frame3, text="Select", corner_radius=6,
-                width=100, command=lambda: [])
+                width=100, command=lambda: [buy_ticket("Aluminium")])
             botton_aluminium.place(x=10, y=120)
 
             frame4 = customtkinter.CTkFrame(master=frame, width=120,
@@ -281,7 +589,7 @@ def button2_click():
             botton_more_info_diamond.place(x=10, y=80)
             botton_diamond = customtkinter.CTkButton(
                 master=frame4, text="Select", corner_radius=6,
-                width=100, command=lambda: [])
+                width=100, command=lambda: [buy_ticket("Diamond")])
             botton_diamond.place(x=10, y=120)
 
             frame5 = customtkinter.CTkFrame(master=frame, width=120,
@@ -301,10 +609,11 @@ def button2_click():
             botton_more_info_premium.place(x=10, y=80)
             botton_premium = customtkinter.CTkButton(
                 master=frame5, text="Select", corner_radius=6,
-                width=100, command=lambda: [])
+                width=100, command=lambda: [(buy_ticket("Premium"))])
             botton_premium.place(x=10, y=120)
         else:
-            messagebox.showwarning(title="Error no dates taken", message="""
+            messagebox.showwarning(title="Error no dates taken",
+                                   message="""
     You may took a wrong selection
     Please select a correct travel date""")
 
@@ -340,12 +649,13 @@ def first_window():
     bg_image.pack()
 
     # now we gona do a frame
+
     frame = customtkinter.CTkFrame(
-                                    master=bg_image, width=320,
-                                    height=360, corner_radius=15)
+        master=bg_image, width=320, height=360, corner_radius=15)
     frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
     # now we goona do the options for the user
+
     option1 = customtkinter.CTkLabel(master=frame,
                                      text="Welcome please select an option",
                                      font=("Century Gothic", 15))
@@ -357,18 +667,15 @@ def first_window():
     customtkinter.CTkLabel(frame, image=image1).place(x=120, y=70)
 
     # now we goona do the buttons options for the user
-    button1 = customtkinter.CTkButton(master=frame,
-                                      width=220,
+
+    button1 = customtkinter.CTkButton(master=frame, width=220,
                                       text="Chek-in",
-                                      corner_radius=6,
-                                      command=botton1_click)
+                                      corner_radius=6, command=button1_click)
     button1.place(x=50, y=150)
 
-    button2 = customtkinter.CTkButton(master=frame,
-                                      width=220,
+    button2 = customtkinter.CTkButton(master=frame, width=220,
                                       text="Buy a ticket",
-                                      corner_radius=6,
-                                      command=button2_click)
+                                      corner_radius=6, command=botton2_click)
     button2.place(x=50, y=250)
 
     app.mainloop()
@@ -380,14 +687,15 @@ def first_window():
     bg_image = tkinter.Label(app, image=image_path)
     bg_image.pack()
     ########################################
-
     # now we gona do a frame
+
     frame = customtkinter.CTkFrame(app)
     frame.place(relx=0.5, rely=0.5, anchor="center")
 
     # now we goona do the options for the user
+
     option1 = customtkinter.CTkLabel(master=frame,
-                                     text="Welcome, pls select an option",
+                                     text="Welcome, please select an option",
                                      font=("Century Gothic", 15))
     option1.place(x=10, y=45)
 
